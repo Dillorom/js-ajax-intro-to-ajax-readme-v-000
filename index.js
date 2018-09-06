73c9d2d129b502b7679ce39af05fde1b42a05d86
Dillorom <dabdulloeva@gmail.com>
2018-09-06 11:58:19 -0700

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ function showRepositories() {
       .join('')}</ul>`;
     document.getElementById('repositories').innerHTML = repoList;
   }
-
+  function getRepositories() {
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', showRepositories);
+    req.open('GET', 'https://api.github.com/users/octocat/repos');
+    req.send();
+  }
+  
   function getCommits(el) {
     const name = el.dataset.repo;
     const req = new XMLHttpRequest();
